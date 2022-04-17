6c2c5ea1d725595e1ef610000d5003c915c1d5b5
reem-elbakry <reemmousa1995@gmail.com>
2022-04-17 19:52:46 +0200

    add slug col in db

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,14 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
 
 class Post extends Model
 {
     use HasFactory;
+
     protected $fillable = ['title', 'description', 'user_id'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+
+    use Sluggable;
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
